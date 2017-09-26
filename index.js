a56a51af9146bd4e660aa992a4e2848a8713c791
atulbipin <atulbipin@gmail.com>
2017-09-25 21:00:39 -0700

    fixed start of oauth flow

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 5001;
 const app = express();
 
-app.get('/', (req, res) => {
-   res.send('Hello World!');
-});
+require('./routes/authRoutes')(app);
 
 app.listen(PORT, () => {
    console.log('Listening on port 5001');
