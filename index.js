35be096f42bbf01d8f8a69758b4fb5001e729695
atulbipin <atulbipin@gmail.com>
2017-09-25 21:10:41 -0700

    fixed errors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+require('./services/passport');
 
-const PORT = process.env.PORT || 5001;
 const app = express();
 
 require('./routes/authRoutes')(app);
 
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
    console.log('Listening on port 5001');
 });
--- services/passport.js
+++ services/passport.js
@@ -3,15 +3,16 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 
 passport.use(
-   new GoogleStrategy({
-      clientID: keys.oauth.google.clientID,
-      clientSecret: keys.oauth.google.clientSecret,
-      callbackURL: '/auth/google/callback'
-   }, {
+   new GoogleStrategy(
+      {
+         clientID: keys.oauth.google.clientID,
+         clientSecret: keys.oauth.google.clientSecret,
+         callbackURL: '/auth/google/callback'
+      },
       (accessToken, refreshToken, profile, done) => {
          console.log('accessToken', accessToken);
          console.log('refreshToken', refreshToken);
          console.log('profile', profile);
       }
-   })
+   )
 );
