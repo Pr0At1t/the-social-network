ac7bab95dad2c6feb905a5cd6fba05991edc27fc
atulbipin <atulbipin@gmail.com>
2017-10-15 08:11:04 -0700

    Added dependencies for OAuth Google

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-   console.log('Listening on port 5001');
+    console.log('Listening on port 5001');
 });
